865b8c1955b6858f35b48466a65f907905984c3b
firefly <161492066+TaygoCezar@users.noreply.github.com>
2024-07-07 02:09:36 -0300

    Update sensor.ultrasonico.ino

--- sensor.ultrasonico.ino
+++ sensor.ultrasonico.ino
@@ -4,7 +4,7 @@
 
 
 LiquidCrystal_I2C lcd(0x27,16,2);
-Ultrasonic ultrasonic(12, 13);
+Ultrasonic ultrasonic(12, 13);  //O sensor ultrassônico mede distância entre 2cm e 4 metros, o sensor retorna as medidas em cm
 int distance;
  
 void setup()
@@ -18,9 +18,9 @@ void loop()
 {
 lcd.setCursor(0,0);
 distance = ultrasonic.read();
-lcd.print("      ");
-lcd.print(distance);    //Exibe no display as medidas
-lcd.print(" cm");
-delay(500);
+lcd.print("      ");     
+lcd.print(distance);    //Exibe no display apenas a distância medida sem identificação da medida de comprimento usada
+lcd.print(" cm");       //Adiciona a terminação 'cm' para o melhor entendimento da distância calculada
+delay(500);             //Adiciona um atraso de 0,5s na impressão da distância
 lcd.clear();
   }
